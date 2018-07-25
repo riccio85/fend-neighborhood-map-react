454be338ff8f340e202212285c875fbff9b09a05
riccio85 <rihanna.ke@gmail.com>
2018-07-25 14:47:19 +0200

    starter code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class NeighborApp extends React.Component {
+  render() {
+    return (
+      <div className="app">
+          HEllo mapss
+      </div>
+    )
+  }
+}
+
+export default NeighborApp
